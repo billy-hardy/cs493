e11a76f3d308bf33dcaf24fbcf9f42e1e11b3076
Billy Hardy <hardywb@leidos.com>
2015-12-06 19:01:51 -0500

    tyesfa

--- logistic_regression/lr.py
+++ logistic_regression/lr.py
@@ -1,5 +1,6 @@
 from sklearn import linear_model
 import pandas as pd
-print pd.read_csv('../gps_training_10/task3edges_10.txt', sep=" ")
-#clf = linear_model.LinearRegression()
-
+data = pd.read_csv('../gps_training_10/task3edges_10.txt', sep=" ", names=["timestamp", "2ndNodeId", "1stNodeId", "aveVelocity"])
+data["timestamp"]
+clf = linear_model.LinearRegression()
+#clf.fit(data.['timestamp']
